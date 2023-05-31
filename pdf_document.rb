65f2019ed0ddb00baf774ca6ee932e014c525986
Jason Swett <jason@mhcare.com>
2023-05-31 16:44:38 -0400

    Get stuff working more.

---
+++ pdf_document.rb
@@ -0,0 +1,24 @@
+require "prawn"
+require "prawn/table"
+
+class PDFDocument < Prawn::Document
+  COLUMN_COUNT = 3
+  ROW_COUNT = 10
+  FONT_SIZE = 10 # Adjust the font size as desired
+
+  def initialize(recipients, options = {})
+    super(options)
+    Prawn::Fonts::AFM.hide_m17n_warning = true
+    create_labels(recipients)
+  end
+
+  def create_labels(recipients)
+    font_size(FONT_SIZE) do
+      rows = recipients.each_slice(COLUMN_COUNT).map do |group|
+        group.map { |recipient| "#{recipient[:name]}\n#{recipient[:address]}" }
+      end
+
+      table(rows)
+    end
+  end
+end
--- program.rb
+++ program.rb
@@ -1,26 +1,55 @@
-require 'net/http'
-require 'uri'
-require 'json'
 require "pry"
+require "json"
+require "net/http"
+require_relative "pdf_document"
 
 FORM_ID = 28
-uri = URI.parse("https://www.codewithjason.com/wp-json/gf/v2/entries?form_ids=#{FORM_ID}")
+PAGE_SIZE = 50
 
-request = Net::HTTP::Get.new(uri)
-request.basic_auth(ENV["GF_CONSUMER_KEY"], ENV["GF_CONSUMER_SECRET"])
+offset = 0
+total_count = nil
 
-response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-  http.request(request)
+recipients = []
+print "Downloading recipients..."
+
+loop do
+  uri = URI.parse("https://www.codewithjason.com/wp-json/gf/v2/entries")
+  uri.query = URI.encode_www_form({
+    'form_ids' => FORM_ID,
+    'paging[offset]' => offset,
+    'paging[page_size]' => PAGE_SIZE,
+  })
+
+  request = Net::HTTP::Get.new(uri)
+  request.basic_auth(ENV["GF_CONSUMER_KEY"], ENV["GF_CONSUMER_SECRET"])
+
+  response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+    http.request(request)
+  end
+
+  if !response.code.to_i == 200
+    raise "HTTP Request failed (status code #{response.code})"
+  end
+
+  response_json = JSON.parse(response.body)
+  total_count ||= response_json["total_count"]
+
+  recipients += response_json["entries"].map do |entry|
+    {
+      name: entry["1"],
+      address: entry["2"]
+    }
+  end
+
+  offset += PAGE_SIZE
+  print "."
+  break if offset >= total_count
 end
 
-if !response.code.to_i == 200
-  raise "HTTP Request failed (status code #{response.code})"
-end
+puts
+puts "Rendering PDF..."
 
-JSON.parse(response.body)["entries"].each do |entry|
-  name = entry["1"]
-  address = entry["2"]
-  puts "-" * 20
-  puts name
-  puts address
-end
+pdf_document = PDFDocument.new(recipients)
+pdf_document.render_file("labels.pdf")
+
+puts "Done"
