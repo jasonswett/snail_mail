de389ea2138979720fe0fa22b92fc65a0addbaf5
Jason Swett <jason@mhcare.com>
2023-06-01 22:13:42 -0400

    Extract constant.

--- spec/program_spec.rb
+++ spec/program_spec.rb
@@ -2,6 +2,8 @@ require "webmock/rspec"
 
 require_relative "../program"
 
+ENTRIES_ENDPOINT_URL = "https://www.codewithjason.com/wp-json/gf/v2/entries"
+
 describe "#run" do
   it "works" do
     response_body = {
@@ -11,7 +13,7 @@ describe "#run" do
 
     stub_request(
       :get,
-      "https://www.codewithjason.com/wp-json/gf/v2/entries?form_ids=28&paging%5Boffset%5D=0&paging%5Bpage_size%5D=50"
+      "#{ENTRIES_ENDPOINT_URL}?form_ids=28&paging%5Boffset%5D=0&paging%5Bpage_size%5D=50"
     ).to_return(
       status: 200,
       body: response_body.to_json.to_s,
