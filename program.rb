539ee3a1d7e08e662f5614bfa83ae77f586be075
Jason Swett <jason@mhcare.com>
2023-06-01 21:28:31 -0400

    Refactoring.

--- program.rb
+++ program.rb
@@ -6,50 +6,54 @@ require_relative "label_document"
 FORM_ID = 28
 PAGE_SIZE = 50
 
-offset = 0
-total_count = nil
+def run
+  offset = 0
+  total_count = nil
 
-recipients = []
-print "Downloading recipients..."
+  recipients = []
+  print "Downloading recipients..."
 
-loop do
-  uri = URI.parse("https://www.codewithjason.com/wp-json/gf/v2/entries")
-  uri.query = URI.encode_www_form({
-    'form_ids' => FORM_ID,
-    'paging[offset]' => offset,
-    'paging[page_size]' => PAGE_SIZE,
-  })
+  loop do
+    uri = URI.parse("https://www.codewithjason.com/wp-json/gf/v2/entries")
+    uri.query = URI.encode_www_form({
+      'form_ids' => FORM_ID,
+      'paging[offset]' => offset,
+      'paging[page_size]' => PAGE_SIZE,
+    })
 
-  request = Net::HTTP::Get.new(uri)
-  request.basic_auth(ENV["GF_CONSUMER_KEY"], ENV["GF_CONSUMER_SECRET"])
+    request = Net::HTTP::Get.new(uri)
+    request.basic_auth(ENV["GF_CONSUMER_KEY"], ENV["GF_CONSUMER_SECRET"])
 
-  response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-    http.request(request)
+    response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+      http.request(request)
+    end
+
+    if !response.code.to_i == 200
+      raise "HTTP Request failed (status code #{response.code})"
+    end
+
+    response_json = JSON.parse(response.body)
+    total_count ||= response_json["total_count"]
+
+    recipients += response_json["entries"].map do |entry|
+      {
+        name: entry["1"],
+        address: entry["2"]
+      }
+    end
+
+    offset += PAGE_SIZE
+    print "."
+    break if offset >= total_count
   end
 
-  if !response.code.to_i == 200
-    raise "HTTP Request failed (status code #{response.code})"
-  end
+  puts
+  puts "Rendering PDF..."
 
-  response_json = JSON.parse(response.body)
-  total_count ||= response_json["total_count"]
+  label_document = LabelDocument.new(recipients)
+  label_document.render_file("labels.pdf")
 
-  recipients += response_json["entries"].map do |entry|
-    {
-      name: entry["1"],
-      address: entry["2"]
-    }
-  end
-
-  offset += PAGE_SIZE
-  print "."
-  break if offset >= total_count
+  puts "Done"
 end
 
-puts
-puts "Rendering PDF..."
-
-label_document = LabelDocument.new(recipients)
-label_document.render_file("labels.pdf")
-
-puts "Done"
+run
