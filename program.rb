50bf525e44dc5232fc7443a677fb05192fdc3f52
Jason Swett <jason@mhcare.com>
2023-06-07 13:57:36 -0400

    Separate CSV writing from label writing.

--- program.rb
+++ program.rb
@@ -2,14 +2,16 @@ require_relative "recipient_list"
 require_relative "recipient_csv"
 require_relative "label_document"
 
+puts
+puts "Writing CSV..."
+
 recipient_csv = RecipientCSV.new(recipients)
-recipient_csv.write
-exit
+filename = recipient_csv.write
 
 puts
 puts "Rendering PDF..."
 
-label_document = LabelDocument.new(recipients)
+label_document = LabelDocument.new(RecipientCSV.from_csv(filename))
 label_document.render_file("labels.pdf")
 
 puts "Done"
--- recipient_csv.rb
+++ recipient_csv.rb
@@ -6,11 +6,26 @@ class RecipientCSV
   end
 
   def write
-    CSV.open("recipients.csv", "wb") do |csv|
+    filename = "recipients.csv"
+
+    CSV.open(filename, "wb") do |csv|
       csv << ["Name", "Address"]
       @recipients.each do |recipient|
         csv << [recipient[:name], recipient[:address]]
       end
     end
+
+    filename
+  end
+
+  def self.from_csv(filename)
+    recipients = []
+    CSV.foreach(filename, headers: true) do |row|
+      recipients << {
+        name: row["Name"],
+        address: row["Address"]
+      }
+    end
+    recipients
   end
 end
