6f22048674254caca02c60e7c4bca29078fca538
Jason Swett <jason@mhcare.com>
2023-06-07 13:40:39 -0400

    Fix PDF rendering.

--- program.rb
+++ program.rb
@@ -1,4 +1,10 @@
 require_relative "recipient_list"
 require_relative "label_document"
 
+puts
+puts "Rendering PDF..."
+
 label_document = LabelDocument.new(recipients)
+label_document.render_file("labels.pdf")
+
+puts "Done"
