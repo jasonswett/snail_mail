25e1a0835474056097e6c1f00808d86e60bce4e0
Jason Swett <jason@mhcare.com>
2023-06-11 09:15:04 -0400

    Make recipient list test easier to understand.

--- spec/recipient_list_spec.rb
+++ spec/recipient_list_spec.rb
@@ -4,9 +4,9 @@ require_relative "../recipient_list"
 
 ENTRIES_ENDPOINT_URL = "https://www.codewithjason.com/wp-json/gf/v2/entries"
 
-describe "#consuming recipients" do
+describe "consuming recipients from the WordPress API" do
   before do
-    response_body = {
+    wordpress_response_body = {
       "entries" => [
         {
           "1" => "Jason Swett",
@@ -16,20 +16,24 @@ describe "#consuming recipients" do
       "total_count" => 1
     }
 
+    stub_address_request(wordpress_response_body)
+  end
+
+  it "formats the response into a hash" do
+    expect(recipients.first).to include(
+      name: "Jason Swett",
+      address: "16601 Myers Lake Ave"
+    )
+  end
+
+  def stub_address_request(wordpress_response_body)
     stub_request(
       :get,
       "#{ENTRIES_ENDPOINT_URL}?form_ids=28&paging%5Boffset%5D=0&paging%5Bpage_size%5D=50"
     ).to_return(
       status: 200,
-      body: response_body.to_json.to_s,
+      body: wordpress_response_body.to_json.to_s,
       headers: {}
-    )
-  end
-
-  it "includes the recipient" do
-    expect(recipients.first).to include(
-      name: "Jason Swett",
-      address: "16601 Myers Lake Ave"
     )
   end
 end
