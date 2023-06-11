819662818662f4b9be2592b99f226041db21808f
Jason Swett <jason@mhcare.com>
2023-06-11 09:21:05 -0400

    Change recipient list into a class.

--- recipient_list.rb
+++ recipient_list.rb
@@ -6,64 +6,66 @@ require_relative "label_document"
 FORM_ID = 28
 PAGE_SIZE = 50
 
-def uri(offset)
-  uri = URI.parse("https://www.codewithjason.com/wp-json/gf/v2/entries")
-  uri.query = URI.encode_www_form({
-    'form_ids' => FORM_ID,
-    'paging[offset]' => offset,
-    'paging[page_size]' => PAGE_SIZE,
-  })
-  uri
-end
-
-def request(uri)
-  request = Net::HTTP::Get.new(uri)
-  request.basic_auth(ENV["GF_CONSUMER_KEY"], ENV["GF_CONSUMER_SECRET"])
-  request
-end
-
-def response(uri, request)
-  Net::HTTP.start(
-    uri.hostname,
-    uri.port,
-    :use_ssl => uri.scheme == 'https'
-  ) do |http|
-    http.request(request)
-  end
-end
-
-def response_json(offset)
-  uri = uri(offset)
-  request = request(uri)
-  response = response(uri, request)
-
-  if !response.code.to_i == 200
-    raise "HTTP Request failed (status code #{response.code})"
+class RecipientList
+  def uri(offset)
+    uri = URI.parse("https://www.codewithjason.com/wp-json/gf/v2/entries")
+    uri.query = URI.encode_www_form({
+      'form_ids' => FORM_ID,
+      'paging[offset]' => offset,
+      'paging[page_size]' => PAGE_SIZE,
+    })
+    uri
   end
 
-  JSON.parse(response.body)
-end
+  def request(uri)
+    request = Net::HTTP::Get.new(uri)
+    request.basic_auth(ENV["GF_CONSUMER_KEY"], ENV["GF_CONSUMER_SECRET"])
+    request
+  end
 
-def recipients
-  offset = 0
-  recipients = []
-  print "Downloading recipients..."
+  def response(uri, request)
+    Net::HTTP.start(
+      uri.hostname,
+      uri.port,
+      :use_ssl => uri.scheme == 'https'
+    ) do |http|
+      http.request(request)
+    end
+  end
 
-  loop do
-    response_json = response_json(offset)
-    total_count = response_json["total_count"]
+  def response_json(offset)
+    uri = uri(offset)
+    request = request(uri)
+    response = response(uri, request)
 
-    recipients += response_json["entries"].map do |entry|
-      {
-        name: entry["1"],
-        address: entry["2"]
-      }
+    if !response.code.to_i == 200
+      raise "HTTP Request failed (status code #{response.code})"
     end
 
-    offset += PAGE_SIZE
-    print "."
-    break if offset >= total_count
+    JSON.parse(response.body)
   end
 
-  recipients
+  def items
+    offset = 0
+    recipients = []
+    print "Downloading recipients..."
+
+    loop do
+      response_json = response_json(offset)
+      total_count = response_json["total_count"]
+
+      recipients += response_json["entries"].map do |entry|
+        {
+          name: entry["1"],
+          address: entry["2"]
+        }
+      end
+
+      offset += PAGE_SIZE
+      print "."
+      break if offset >= total_count
+    end
+
+    recipients
+  end
 end
--- spec/recipient_list_spec.rb
+++ spec/recipient_list_spec.rb
@@ -4,36 +4,40 @@ require_relative "../recipient_list"
 
 ENTRIES_ENDPOINT_URL = "https://www.codewithjason.com/wp-json/gf/v2/entries"
 
-describe "consuming recipients from the WordPress API" do
-  before do
-    wordpress_response_body = {
-      "entries" => [
-        {
-          "1" => "Jason Swett",
-          "2" => "16601 Myers Lake Ave"
-        }
-      ],
-      "total_count" => 1
-    }
+describe RecipientList do
+  describe "consuming recipients from the WordPress API" do
+    before do
+      wordpress_response_body = {
+        "entries" => [
+          {
+            "1" => "Jason Swett",
+            "2" => "16601 Myers Lake Ave"
+          }
+        ],
+        "total_count" => 1
+      }
 
-    stub_address_request(wordpress_response_body)
-  end
+      stub_address_request(wordpress_response_body)
 
-  it "formats the response into a hash" do
-    expect(recipients.first).to include(
-      name: "Jason Swett",
-      address: "16601 Myers Lake Ave"
-    )
-  end
+      @recipient_list = RecipientList.new
+    end
 
-  def stub_address_request(wordpress_response_body)
-    stub_request(
-      :get,
-      "#{ENTRIES_ENDPOINT_URL}?form_ids=28&paging%5Boffset%5D=0&paging%5Bpage_size%5D=50"
-    ).to_return(
-      status: 200,
-      body: wordpress_response_body.to_json.to_s,
-      headers: {}
-    )
+    it "formats the response into a hash" do
+      expect(@recipient_list.items.first).to include(
+        name: "Jason Swett",
+        address: "16601 Myers Lake Ave"
+      )
+    end
+
+    def stub_address_request(wordpress_response_body)
+      stub_request(
+        :get,
+        "#{ENTRIES_ENDPOINT_URL}?form_ids=28&paging%5Boffset%5D=0&paging%5Bpage_size%5D=50"
+      ).to_return(
+        status: 200,
+        body: wordpress_response_body.to_json.to_s,
+        headers: {}
+      )
+    end
   end
 end
