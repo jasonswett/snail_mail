8121d1b931cf2e31669bb192df03c8e2859786f9
Jason Swett <jason@benfranklinlabs.com>
2023-10-31 20:09:09 -0400

    Fix bug.

--- program.rb
+++ program.rb
@@ -12,6 +12,6 @@ csv_filename = recipient_csv.write
 puts
 puts "Rendering PDF..."
 
-system("ruby make_labels.rb #{label_pdf_filename}")
+system("ruby make_labels.rb #{csv_filename}")
 
 puts "Done"
